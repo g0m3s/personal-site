a733f9eb359878a36cad11162cc3734d4071a5f4
g0m3s <gabrielmatosgm999@gmail.com>
2020-05-17 21:23:25 -0300

    modificando texto e-mail

--- js/main.js
+++ js/main.js
@@ -13,14 +13,22 @@ function typeWriter(elemento) {
 
   function enviaEmail(){
 
-    const mail = document.querySelector('#mail')
     const nome = document.querySelector('#nome').value
     const telefone = document.querySelector('#telefone').value
     const mensagem = document.querySelector('#mensagem').value
 
-    mail.href = `mailto:gabrielmatosgm99@gmail.com?subject=Ola!%20preciso%20de%20uma%20ajudinha&body=Ola,%20me%20chamo%20${nome}%20e%20tenho%20uma%20mensagem:%20${mensagem}%20%20meu%20telefone%20e%20${telefone}`
+    if(telefone && nome && mensagem){
 
-    alert(`obrigado pelo contato, ${nome}! em pouco tempo te retornarei o e-mail e espero que consiga te ajudar!.`)
+        mail.href = `mailto:gabrielmatosgm99@gmail.com?subject=Ola!%20preciso%20de%20uma%20ajudinha&body=Oi%20,%20meu%20nome%20e%20${nome}.%0DMeu%20telefone%20e:%20${telefone}%20.%0D${mensagem}`
+
+        alert(`Obrigado pelo contato, ${nome}! Seu app de e-mail abrirá agora e basta clicar em enviar que em pouco tempo te retornarei e espero que consiga te ajudar!.`)
+    
+    }else{
+
+        alert(`Por favor, preencha todos os campos para que possamos ter um melhor contato!`)
+
+    }
+
 
     return 0
 
